a8478d5e8da1a3d2607840feab7031649c00960d
ikechukwu powel <ikechukwupowei21@gmail.com>
2025-09-16 08:07:43 +0100

    config: redux-toolkit set-up

---
+++ src/app/rtk-base/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "@/app/rtk-base/slices/counter-slice";
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
+
+// Types for later use in components
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
