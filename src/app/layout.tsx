2a307dc759835f26659f78cea3700d924781ee17
Okpainmo <okpainmoandrew@gmail.com>
2025-09-17 15:41:06 +0100

    chore: project fonts upgrade

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,20 @@
 import "@/app/styles/globals.css";
 import type { Metadata } from "next";
 import { Providers } from "@/app/rtk-base/providers";
-import { nunito_sans, poppins } from "./utils/font";
+import { nunito_sans, poppins, lato } from './utils/font';
 
 export const metadata: Metadata = {
   title: 'Deeco: The Best Way To Sell Online',
   description: 'Deeco Platform',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
-    <html lang="en" className={`${nunito_sans} ${poppins}`}>
+    <html lang='en' className={`${nunito_sans} ${poppins} ${lato}`}>
       <body>
         <Providers>{children}</Providers>
       </body>
