ad05870b061e4925df9b876b68c3a9b767afa0e1
Okpainmo <okpainmoandrew@gmail.com>
2025-09-17 15:42:24 +0100

    chore: core pages updates

--- src/app/(routes)/(dashboard)/brands/page.tsx
+++ src/app/(routes)/(dashboard)/brands/page.tsx
@@ -2,12 +2,17 @@ import PageRoutesIndicator from "../components/PageRoutesIndicator";
 
 export default function BrandsPage() {
   return (
-    <main className="p-6 min-h-screen flex flex-col">
-      <PageRoutesIndicator pageRoutes="Admin / Analytics Page" pageTitle="Brands Page" />
-      <div className="flex-1 flex items-center justify-center">
-        <div className="text-center">
-          <h2 className="text-2xl font-semibold mb-2">Brands Overview</h2>
-          <p className="text-gray-600">This is some dummy text for the Brands page.</p>
+    <main className='min-h-screen flex flex-col'>
+      <PageRoutesIndicator
+        pageRoutes='Admin / Analytics Page'
+        pageTitle='Brands Page'
+      />
+      <div className='flex-1 flex items-center justify-center'>
+        <div className='text-center'>
+          <h2 className='text-2xl font-semibold mb-2'>Brands Overview</h2>
+          <p className='text-gray-600'>
+            This is some dummy text for the Brands page.
+          </p>
         </div>
       </div>
     </main>
