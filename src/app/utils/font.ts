56c930a7b401fd908946d14310051d8154e42df6
Okpainmo <okpainmoandrew@gmail.com>
2025-09-24 02:45:19 +0100

    chore: extra fonts setup update

--- src/app/utils/font.ts
+++ src/app/utils/font.ts
@@ -1,4 +1,4 @@
-import { Inter, Nunito_Sans, Poppins } from 'next/font/google';
+import { Lato, Nunito_Sans, Poppins } from 'next/font/google';
 
 const nunito_sans_init = Nunito_Sans({
   subsets: ['latin'],
@@ -18,7 +18,7 @@ const lato_init = Lato({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-lato',
-  weight: ['200', '300', '400', '500', '600', '700', '800', '900'],
+  weight: ['100', '300', '400', '700', '900'],
 });
 
 export const nunito_sans = nunito_sans_init.variable;
